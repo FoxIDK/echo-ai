977e90b41d87e6d3932eaf7f6e28714a015bf538
FoxIDK <120978468+FoxIDK@users.noreply.github.com>
2023-05-04 22:07:19 +0100

    Added NLTK packages

--- neuralset.py
+++ neuralset.py
@@ -3,4 +3,9 @@ import subprocess
 install = ["pip", "install", "-r", "requirements.txt"]
 subprocess.run(install)
 
+import nltk
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
 print("[!] REQUIREMENTS INSTALLED\n[!] Please run the: echo-l1.py inside of the directory 'src' to launch Echo.")
